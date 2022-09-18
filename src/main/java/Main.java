71684b203584fc45012eb16e553ee05f462fd945
Vladislav <v.r.ryazantsev@gmail.com>
2022-09-18 20:24:35 +0300

    fx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,9 @@ import java.util.List;
 public class Main {
 
     private static final String url = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
 
     public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
         GetPosts getPosts = new GetPosts(url);
         try (CloseableHttpResponse response = getPosts.createHttpResponse()) {
             List<Post> posts = objectMapper
